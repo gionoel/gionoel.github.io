ca3912c40a87bcffcfbdb4684e7470b594ab6448
Gio Gonzales <gionoelgonzales@gmail.com>
2018-02-14 03:19:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-$(document).ready(function() {
-  $('a').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 300, 'linear');
-  });
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox({
+                    alwaysShowClose: true
+                });
+            });
+
+
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
 });
+
+
+var skillsquare = document.getElementsByClassName("skillicon");
+
+for(var i = 0; i<skillsquare.length; i++){
+    skillsquare[i].addEventListener("mouseover", function(){
+        this.classList.add("bounce");
+    });
+    skillsquare[i].addEventListener("mouseout", function(){
+        this.classList.remove("bounce");
+    });
+    
+}
