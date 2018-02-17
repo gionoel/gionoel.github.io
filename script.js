d9ab7f466816955b049b710e07f79c68695a153c
Gio Gonzales <gionoelgonzales@gmail.com>
2018-02-17 19:34:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,15 +11,20 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
 
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
 
 var skillsquare = document.getElementsByClassName("skillicon");
 
 for(var i = 0; i<skillsquare.length; i++){
     skillsquare[i].addEventListener("mouseover", function(){
-        this.classList.add("bounce");
+        this.classList.add("bounceIn");
+        this.classList.add("infinite");
     });
     skillsquare[i].addEventListener("mouseout", function(){
-        this.classList.remove("bounce");
+        this.classList.remove("bounceIn");
     });
     
 }
